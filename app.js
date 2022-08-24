d923c53a86ce7b3d708d89c9bed63b98edbba8ae
juan-alvarz <juanalvarez0925@gmail.com>
2022-08-23 20:37:38 -0500

    second commit

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ app.use(cors());
 
 const port = process.env.PORT || 3000;
 
+//invocamos a las rutas
+//localhost:/api/
+app.use("/api", require("./routes"));
+
+//ejecuta el back en el puerto
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 dbConnect();
