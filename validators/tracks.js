13ef156e384654eab8d5910bdf3cd5ebe82ed284
juan-alvarz <juanalvarez0925@gmail.com>
2022-08-26 18:05:28 -0500

    almost to init soft delete

---
+++ validators/tracks.js
@@ -0,0 +1,17 @@
+const { check } = require("express-validator");
+const validateResults = require("../utils/handleValidator.js");
+
+const validatorCreateItem = [
+  check("name").exists().notEmpty(),
+  check("album").exists().notEmpty(),
+  check("cover").exists().notEmpty(),
+  check("artist.name").exists().notEmpty(),
+  check("artist.nickname").exists().notEmpty(),
+  check("artist.nationality").exists().notEmpty(),
+  check("duration.start").exists().notEmpty(),
+  check("duration.end").exists().notEmpty(),
+  check("mediaId").exists().notEmpty().isMongoId(),
+  (req, res, next) => validateResults(req, res, next),
+];
+
+module.exports = { validatorCreateItem };
